8d66d61555a2c6094d54f5aa00372fd93fa274af
Antoine <acoudray@student.42.fr>
2019-11-20 11:55:42 +0100

    free WIP

--- srcs/free.c
+++ srcs/free.c
@@ -3,21 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmachena <gmachena@student.42.fr>          +#+  +:+       +#+        */
+/*   By: acoudray <acoudray@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 11:18:37 by gmachena          #+#    #+#             */
-/*   Updated: 2019/11/13 16:42:09 by gmachena         ###   ########.fr       */
+/*   Updated: 2019/11/20 11:51:34 by acoudray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
 
+// void    ft_set_free(t_block *ptr)
+// {
+    
+// }
+
 void    ft_free(void *ptr)
 {
-    printf("bonjour\n");
-    if (!(glob_m))
-    	printf("bonjour\n");
-	ft_malloc(1);
-    //munmap(0, 0);
-    (void)ptr;
+    t_block *metadata;
+    t_block *start;
+    start = glob_m;
+    metadata = ptr - sizeof(t_block);
+    if (metadata->next != NULL)
+    {
+        while (start->next != metadata)
+            start = start->next;
+        start->next = metadata->next;
+    }
+    metadata->free = 1;
 }
